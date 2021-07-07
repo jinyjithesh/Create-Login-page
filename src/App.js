e7f504d032e1ad9f34bbc629a0d3b3b1633d38b5
jinyjithesh <jinyjithesh@gmail.com>
2021-07-07 09:31:32 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import Login from './Components/Login/Login';
 
 
 
-
-
-
 function App() {
   const adminUser={
     emailAddress:"admin@test.com",
-    password:"1234",
+    password:"12345"
 }
-const[user,setUser]=useState({email:"",password:""});
+const[user,setUser]=useState({emailAddress:"",password:""});
 const[error,setError]=useState("");
-const login= details =>{
+const login = details =>{
   console.log(details);
-  if (details.email ==adminUser.emailAddress && details.password==adminUser.password){
+
+  if (details.emailAddress == adminUser.emailAddress && details.password == adminUser.password){
     console.log("Logged in");
     setUser ({
       emailAddress:details.emailAddress,
       password:details.password
     });
   }else{
-    console.log("Details do not match");
+    console.log("email do not match")
+  
     setError("email do not match")
   }
 }
@@ -32,15 +31,17 @@ const Logout =()=>{
   setUser({emailAddress:"",password:""});
 }
   return (
-    <div className="App">
+    
 
- {(user.email !="")?(
-   <div>
-     <h2>welcome,<span>{user.name}</span></h2>
-     <button onClick={Logout}>Logout</button>
-   </div>
- ) : ( <Login login={login} error={console.error()}/>)}
+<div className="App">
  
+{(user.emailAddress !="")?(
+<div>
+<h2>welcome</h2>
+<button onClick={Logout}>Logout</button>
+</div>
+):( <Login login={login} error={error}/>)}
+
     </div>
   );
 }
