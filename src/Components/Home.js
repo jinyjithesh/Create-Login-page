a8d60650f85f2778c16caea45b67a02903080d88
jinyjithesh <jinyjithesh@gmail.com>
2021-07-15 16:04:34 +0530

    home

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BranchList } from './BranchList'
+import { Heading } from './Heading'
+
+export const Home = () => {
+    return (
+        <div>
+            <Heading/>
+            <BranchList/>
+        </div>
+    )
+}
