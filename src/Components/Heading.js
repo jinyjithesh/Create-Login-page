a6e04f8a9ac853bda53e8f0cd43e13d37903c9e0
jinyjithesh <jinyjithesh@gmail.com>
2021-07-15 15:17:23 +0530

    heading

---
+++ src/Components/Heading.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Container, Nav, Navbar, NavbarBrand,NavItem} from 'reactstrap'
+export const Heading = () => {
+    return (
+        <div>
+<Navbar color="dark" dark>
+    <Container>
+        <NavbarBrand href="/">
+            My Branch
+
+        </NavbarBrand>
+        <Nav>
+            <NavItem>
+
+            <Link className="btn btn-primary" to="/add">Add Branch</Link>
+            </NavItem>
+        </Nav>
+    </Container>
+</Navbar>
+        </div>
+    )
+}
