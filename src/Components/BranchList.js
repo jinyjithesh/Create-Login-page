8b40f1737b5d5b5706076edaf3d35853292a4388
jinyjithesh <jinyjithesh@gmail.com>
2021-07-21 13:36:33 +0530

    new

--- src/Components/BranchList.js
+++ src/Components/BranchList.js
@@ -1,131 +1,114 @@
-import React ,{useState,useEffect}from 'react'
+import React, { useState, useEffect} from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
-import { ListGroup, Button, ListGroupItem} from 'reactstrap'
+import { Link } from "react-router-dom";
+import { ListGroup, Button} from "reactstrap";
 export const BranchList = () => {
-    const[branchdata,setBranchData]=useState([]);
-    const[userdata,setUserData]=useState({branchId:"",name:"",address:"",city :"",phone:"",timeZone:"",accessCode:""});
-    
-    const getBranchData =async() =>{
-        try{
-            const data= await axios.get("https://staging.bfitds.com/api/Branch" );
-         
-                console.log(data.data);
-                setBranchData(data.data);
-                
-            }catch(err){
-                console.log(err);
-              };
-        };
-  
+  const [branchdata, setBranchData] = useState([]);
+  const [userdata, setUserData] = useState({
+    branchId: "",
+    name: "",
+    address: "",
+    city: "",
+    phone: "",
+    timeZone: "",
+    accessCode: "",
+  });
+  const token = getToken();
+  const getBranchData = async () => {
+    try {
+      const data = await axios.get("https://staging.bfitds.com/api/Branch");
 
-    useEffect(() => {
-        getBranchData();
-    }, []);
-    
-    
- 
-    const removeHandler= async(id)=>{
-      
-    
-      await axios.delete("https://staging.bfitds.com/api/Branch/76",
-      {
-          branchId:userdata.branchId,
-          name:userdata.name,
-          address:userdata.address,
-          city:userdata.city,
-          phone:userdata.phone,
-          timeZone:userdata.timeZone,
-          accessCode:userdata.accessCode,
-  
-      },
-       )
-      
-      .then((res)=>{
-          
-              if( res.status === 200 &&  res.deletdata.success === true){
-                  setUserData(res.data.userdata.branchId);
-                 
-              console.log(res.userdata)
-             
-          }
-         else{
-             console.log(res.userdata)
-         }
-  
-          })
-          .catch((err)=>{
-              console.log(err);
-          })
-  
+      console.log(data.data);
+      setBranchData(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBranchData();
+  }, []);
+
+  const removeHandler = (id) => {
+    axios
+      .delete(
+        "https://staging.bfitds.com/api/Branch/83",
+        {
+          branchId: userdata.branchId,
+          name: userdata.name,
+          address: userdata.address,
+          city: userdata.city,
+          phone: userdata.phone,
+          timeZone: userdata.timeZone,
+          accessCode: userdata.accessCode,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
         }
-      
-           
-        
- 
-// const removeHandler=(id)=>{
- 
-    
-//     console.log(id)
-//     axios.delete("https://staging.bfitds.com/api/Branch/76")
+      )
 
-//     .then(res=>{
-//         localStorage.setItem("token");
-//         const data = userdata.filter(p=>p.branchId !==id)
-   
-//         if( res.status === 200 &&  res.userdata.success === true){
-//             setUserData(res.data.userdata.branchId);
-//         console.log(res.userdata)
-       
-//     }
-//    else{
-//        console.log(res.userdata)
-//    }
+      .then((res) => {
+        if (res.data.statusText === "OK") {
+          setUserData(res.data.userdata.branchId);
+          setUserData((userData) =>
+            userData.filter((item, index) => index !== id)
+          );
 
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//     })
-// }
+          console.log(res.userdata);
+        } else {
+          console.log(res.userdata);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <ListGroup className="mt-4">
-           
-                     <table  className="table table-striped">
-                     
+  return (
+    <ListGroup className="mt-4">
+      <table className="table table-striped">
         <thead>
-   
-            <tr>
-                <td>No</td>
-                <td>Name</td>
-                <td>Address</td>
-                <td>City</td>
-                <td>Phone</td>
-                <td>TimeZone</td>
-            </tr>
-          
+          <tr>
+            <td>No</td>
+            <td>Name</td>
+            <td>Address</td>
+            <td>City</td>
+            <td>Phone</td>
+            <td>TimeZone</td>
+          </tr>
         </thead>
-      
-    {branchdata.map((p,id) =>(
-    <tbody>
-       
-    <tr key={id}> 
-    <td>{p.branchId}</td>
-    <td>{p.name}</td>
-    <td> {p.address}</td>
-    <td> {p.city}</td>
-    <td> {p.phone}</td>
-    <td>{p.timeZone}</td>
-    <td> <Link className="btn btn-warning mr-1  " to={'/edit/:{id}'} >edit</Link></td>
-    <td>  <Button onClick={()=>removeHandler(id)} color="danger" >Delete</Button></td>
-    </tr>
-   
-    </tbody>
- ))}
-</table>
 
-        </ListGroup>
-     
-      
-    )
-}
+        {branchdata.map((p, id) => (
+          <tbody>
+            <tr key={id}>
+              <td>{p.branchId}</td>
+              <td>{p.name}</td>
+              <td> {p.address}</td>
+              <td> {p.city}</td>
+              <td> {p.phone}</td>
+              <td>{p.timeZone}</td>
+              <td>
+                {" "}
+                <Link className="btn btn-warning mr-1  " to={"/edit/:{id}"}>
+                  edit
+                </Link>
+              </td>
+              <td>
+                {" "}
+                <Button onClick={(e) => removeHandler(e, id)} color="danger">
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </ListGroup>
+  );
+};
+export const getToken = () => {
+  return localStorage.getItem("token") || null;
+};
+
