26a53868c5b456b19d41e3217f87f39bb8f70949
jinyjithesh <jinyjithesh@gmail.com>
2021-07-21 13:38:16 +0530

    new

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { Container, Nav, Navbar, NavbarBrand,NavItem} from 'reactstrap'
+
 export const Heading = () => {
     return (
         <div>
 <Navbar color="dark" dark>
     <Container>
-        <NavbarBrand href="/">
+    <NavbarBrand href="/">
             My Branch
 
         </NavbarBrand>
+   
+      
         <Nav>
             <NavItem>
 
