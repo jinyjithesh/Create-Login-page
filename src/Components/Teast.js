1d6be57bcce45bae2b3db1c30a28a2e57286a4de
jinyjithesh <jinyjithesh@gmail.com>
2021-07-28 00:39:45 +0530

    data

---
+++ src/Components/Teast.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+import data from "../data.json";
+export const Teast = () => {
+  const [step, setStep] = useState(0);
+
+  const onNext = () => {
+    console.log();
+    if (data.length > step) setStep(step + 1);
+  };
+  const onBack = () => {
+    if (step > 0) setStep(step - 1);
+  };
+
+  return (
+    <div>
+      <form className="border border-primary ">
+        <div>
+          <div>
+            <label>Question</label>
+            <p>
+              {data[step] && data[step].description}
+              <br />
+            </p>
+            <label>Options</label>
+            {data[step].choices.map((p) => (
+              <div>
+                <p>
+                  {" "}
+                  <input type="radio" />
+                  {p.code} .{p.description}
+                </p>
+              </div>
+            ))}{" "}
+            <br />
+            <br />
+            {step > 0 && (
+              <Button
+                className="btn btn-primary mr-6"
+                disabled={step.false}
+                onClick={onBack}
+              >
+                Previous
+              </Button>
+            )}
+            {step < data.length - 1 && (
+              <Button
+                className="btn btn-warning mr-6"
+                disabled={step.true}
+                onClick={onNext}
+              >
+                Next
+              </Button>
+            )}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
