f04fd5e11465f02f6693996ee8cf06078c3c7f57
jinyjithesh <jinyjithesh@gmail.com>
2021-07-28 00:49:00 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Login from "./Components/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Test } from "./Components/Test";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Test/>
     </div>
   );
 }
